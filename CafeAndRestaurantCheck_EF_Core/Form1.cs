aa1e95e547cc22aaff954604113d810ab65ea185
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-12-07 14:52:59 +0300

    Add project files.

---
+++ CafeAndRestaurantCheck_EF_Core/Form1.cs
@@ -0,0 +1,10 @@
+namespace CafeAndRestaurantCheck_EF_Core
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CafeAndRestaurantCheck_EF_Core/Program.cs
@@ -0,0 +1,15 @@
+namespace CafeAndRestaurantCheck_EF_Core
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
