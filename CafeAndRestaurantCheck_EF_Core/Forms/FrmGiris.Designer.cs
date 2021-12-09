a0818d534db3bea2f8d4cf9745c63d10a31ddca0
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-12-09 22:16:12 +0300

    FrmGiris eklendi.

---
+++ CafeAndRestaurantCheck_EF_Core/Forms/FrmGiris.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CafeAndRestaurantCheck_EF_Core.Forms
+{
+    partial class FrmGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel6 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnYonetici = new System.Windows.Forms.Button();
+            this.btnPersonel = new System.Windows.Forms.Button();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel6
+            // 
+            this.tableLayoutPanel6.ColumnCount = 2;
+            this.tableLayoutPanel6.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel6.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel6.Location = new System.Drawing.Point(3, 7);
+            this.tableLayoutPanel6.Name = "tableLayoutPanel6";
+            this.tableLayoutPanel6.RowCount = 2;
+            this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel6.Size = new System.Drawing.Size(1, 1);
+            this.tableLayoutPanel6.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnKapat, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(868, 481);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.btnPersonel, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnYonetici, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(862, 378);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // btnYonetici
+            // 
+            this.btnYonetici.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnYonetici.BackColor = System.Drawing.Color.Chocolate;
+            this.btnYonetici.ForeColor = System.Drawing.Color.White;
+            this.btnYonetici.Location = new System.Drawing.Point(223, 150);
+            this.btnYonetici.Margin = new System.Windows.Forms.Padding(11, 8, 11, 8);
+            this.btnYonetici.Name = "btnYonetici";
+            this.btnYonetici.Size = new System.Drawing.Size(197, 77);
+            this.btnYonetici.TabIndex = 47;
+            this.btnYonetici.Text = "YÖNETİCİ";
+            this.btnYonetici.UseVisualStyleBackColor = false;
+            // 
+            // btnPersonel
+            // 
+            this.btnPersonel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnPersonel.BackColor = System.Drawing.Color.Chocolate;
+            this.btnPersonel.ForeColor = System.Drawing.Color.White;
+            this.btnPersonel.Location = new System.Drawing.Point(442, 151);
+            this.btnPersonel.Margin = new System.Windows.Forms.Padding(11, 8, 11, 8);
+            this.btnPersonel.Name = "btnPersonel";
+            this.btnPersonel.Size = new System.Drawing.Size(199, 76);
+            this.btnPersonel.TabIndex = 48;
+            this.btnPersonel.Text = "PERSONEL";
+            this.btnPersonel.UseVisualStyleBackColor = false;
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnKapat.BackColor = System.Drawing.Color.Chocolate;
+            this.btnKapat.ForeColor = System.Drawing.Color.White;
+            this.btnKapat.Location = new System.Drawing.Point(11, 438);
+            this.btnKapat.Margin = new System.Windows.Forms.Padding(11, 8, 11, 8);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(107, 35);
+            this.btnKapat.TabIndex = 48;
+            this.btnKapat.Text = "ÇIKIŞ";
+            this.btnKapat.UseVisualStyleBackColor = false;
+            // 
+            // FrmGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 481);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FrmGiris";
+            this.Text = "FrmGiris";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel6;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button btnKapat;
+        private Button btnPersonel;
+        private Button btnYonetici;
+    }
+}
