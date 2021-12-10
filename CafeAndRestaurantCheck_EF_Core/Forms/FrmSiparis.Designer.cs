e78fd969f714b387c52e5f4510f66decc112fccc
Berna UZUNOĞLU <68403247+BernaUzunoglu@users.noreply.github.com>
2021-12-10 09:00:09 +0300

    Siparis form ve sepet eklendi.

--- CafeAndRestaurantCheck_EF_Core/Forms/FrmSiparis.Designer.cs
+++ CafeAndRestaurantCheck_EF_Core/Forms/FrmSiparis.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanelMenuUrun = new System.Windows.Forms.TableLayoutPanel();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelMenuUrunler = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanelurunler = new System.Windows.Forms.TableLayoutPanel();
             this.flwpUrunller = new System.Windows.Forms.FlowLayoutPanel();
             this.flwpMenu = new System.Windows.Forms.FlowLayoutPanel();
@@ -37,41 +37,44 @@
             this.btn_SiparisAl = new System.Windows.Forms.Button();
             this.btnAdisyonKapat = new System.Windows.Forms.Button();
             this.btnGeri = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lstCart = new System.Windows.Forms.ListView();
             this.tableLayoutPanelMenuUrun.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanelMenuUrunler.SuspendLayout();
             this.tableLayoutPanelurunler.SuspendLayout();
             this.tableLayoutPanelbutonlar.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanelMenuUrun
             // 
             this.tableLayoutPanelMenuUrun.ColumnCount = 1;
             this.tableLayoutPanelMenuUrun.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanelMenuUrun.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanelMenuUrun.Controls.Add(this.tableLayoutPanelMenuUrunler, 0, 0);
             this.tableLayoutPanelMenuUrun.Controls.Add(this.tableLayoutPanelbutonlar, 0, 1);
             this.tableLayoutPanelMenuUrun.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelMenuUrun.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanelMenuUrun.Name = "tableLayoutPanelMenuUrun";
             this.tableLayoutPanelMenuUrun.RowCount = 2;
-            this.tableLayoutPanelMenuUrun.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
-            this.tableLayoutPanelMenuUrun.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanelMenuUrun.Size = new System.Drawing.Size(1374, 644);
+            this.tableLayoutPanelMenuUrun.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanelMenuUrun.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanelMenuUrun.Size = new System.Drawing.Size(1735, 811);
             this.tableLayoutPanelMenuUrun.TabIndex = 0;
             // 
-            // tableLayoutPanel2
+            // tableLayoutPanelMenuUrunler
             // 
-            this.tableLayoutPanel2.ColumnCount = 2;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
-            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanelurunler, 1, 0);
-            this.tableLayoutPanel2.Controls.Add(this.flwpMenu, 0, 0);
-            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 1;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1368, 509);
-            this.tableLayoutPanel2.TabIndex = 0;
+            this.tableLayoutPanelMenuUrunler.ColumnCount = 2;
+            this.tableLayoutPanelMenuUrunler.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelMenuUrunler.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanelMenuUrunler.Controls.Add(this.tableLayoutPanelurunler, 1, 0);
+            this.tableLayoutPanelMenuUrunler.Controls.Add(this.flwpMenu, 0, 0);
+            this.tableLayoutPanelMenuUrunler.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelMenuUrunler.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanelMenuUrunler.Name = "tableLayoutPanelMenuUrunler";
+            this.tableLayoutPanelMenuUrunler.RowCount = 1;
+            this.tableLayoutPanelMenuUrunler.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMenuUrunler.Size = new System.Drawing.Size(1729, 683);
+            this.tableLayoutPanelMenuUrunler.TabIndex = 0;
             // 
             // tableLayoutPanelurunler
             // 
@@ -79,12 +82,13 @@
             this.tableLayoutPanelurunler.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.tableLayoutPanelurunler.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanelurunler.Controls.Add(this.flwpUrunller, 0, 0);
+            this.tableLayoutPanelurunler.Controls.Add(this.tableLayoutPanel1, 1, 0);
             this.tableLayoutPanelurunler.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanelurunler.Location = new System.Drawing.Point(276, 3);
+            this.tableLayoutPanelurunler.Location = new System.Drawing.Point(348, 3);
             this.tableLayoutPanelurunler.Name = "tableLayoutPanelurunler";
             this.tableLayoutPanelurunler.RowCount = 1;
             this.tableLayoutPanelurunler.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanelurunler.Size = new System.Drawing.Size(1089, 503);
+            this.tableLayoutPanelurunler.Size = new System.Drawing.Size(1378, 677);
             this.tableLayoutPanelurunler.TabIndex = 0;
             // 
             // flwpUrunller
@@ -92,7 +96,7 @@
             this.flwpUrunller.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flwpUrunller.Location = new System.Drawing.Point(3, 3);
             this.flwpUrunller.Name = "flwpUrunller";
-            this.flwpUrunller.Size = new System.Drawing.Size(756, 497);
+            this.flwpUrunller.Size = new System.Drawing.Size(958, 671);
             this.flwpUrunller.TabIndex = 0;
             // 
             // flwpMenu
@@ -101,7 +105,7 @@
             this.flwpMenu.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flwpMenu.Location = new System.Drawing.Point(3, 3);
             this.flwpMenu.Name = "flwpMenu";
-            this.flwpMenu.Size = new System.Drawing.Size(267, 503);
+            this.flwpMenu.Size = new System.Drawing.Size(339, 677);
             this.flwpMenu.TabIndex = 1;
             // 
             // tableLayoutPanelbutonlar
@@ -114,11 +118,11 @@
             this.tableLayoutPanelbutonlar.Controls.Add(this.btnAdisyonKapat, 0, 0);
             this.tableLayoutPanelbutonlar.Controls.Add(this.btnGeri, 0, 0);
             this.tableLayoutPanelbutonlar.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanelbutonlar.Location = new System.Drawing.Point(3, 518);
+            this.tableLayoutPanelbutonlar.Location = new System.Drawing.Point(3, 692);
             this.tableLayoutPanelbutonlar.Name = "tableLayoutPanelbutonlar";
             this.tableLayoutPanelbutonlar.RowCount = 1;
             this.tableLayoutPanelbutonlar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanelbutonlar.Size = new System.Drawing.Size(1368, 123);
+            this.tableLayoutPanelbutonlar.Size = new System.Drawing.Size(1729, 116);
             this.tableLayoutPanelbutonlar.TabIndex = 1;
             // 
             // btn_SiparisAl
@@ -130,11 +134,11 @@
             this.btn_SiparisAl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_SiparisAl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btn_SiparisAl.ForeColor = System.Drawing.Color.White;
-            this.btn_SiparisAl.Location = new System.Drawing.Point(915, 4);
+            this.btn_SiparisAl.Location = new System.Drawing.Point(1155, 4);
             this.btn_SiparisAl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_SiparisAl.Name = "btn_SiparisAl";
             this.btn_SiparisAl.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.btn_SiparisAl.Size = new System.Drawing.Size(450, 115);
+            this.btn_SiparisAl.Size = new System.Drawing.Size(571, 108);
             this.btn_SiparisAl.TabIndex = 9;
             this.btn_SiparisAl.Text = "SİPARİŞ AL";
             this.btn_SiparisAl.UseVisualStyleBackColor = false;
@@ -148,11 +152,11 @@
             this.btnAdisyonKapat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAdisyonKapat.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnAdisyonKapat.ForeColor = System.Drawing.Color.White;
-            this.btnAdisyonKapat.Location = new System.Drawing.Point(459, 4);
+            this.btnAdisyonKapat.Location = new System.Drawing.Point(579, 4);
             this.btnAdisyonKapat.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnAdisyonKapat.Name = "btnAdisyonKapat";
             this.btnAdisyonKapat.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.btnAdisyonKapat.Size = new System.Drawing.Size(450, 115);
+            this.btnAdisyonKapat.Size = new System.Drawing.Size(570, 108);
             this.btnAdisyonKapat.TabIndex = 8;
             this.btnAdisyonKapat.Text = "HESAP";
             this.btnAdisyonKapat.UseVisualStyleBackColor = false;
@@ -170,24 +174,48 @@
             this.btnGeri.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnGeri.Name = "btnGeri";
             this.btnGeri.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.btnGeri.Size = new System.Drawing.Size(450, 115);
+            this.btnGeri.Size = new System.Drawing.Size(570, 108);
             this.btnGeri.TabIndex = 7;
             this.btnGeri.Text = "GERİ";
             this.btnGeri.UseVisualStyleBackColor = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lstCart, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(967, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(408, 671);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // lstCart
+            // 
+            this.lstCart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstCart.Location = new System.Drawing.Point(3, 3);
+            this.lstCart.Name = "lstCart";
+            this.lstCart.Size = new System.Drawing.Size(402, 597);
+            this.lstCart.TabIndex = 0;
+            this.lstCart.UseCompatibleStateImageBehavior = false;
             // 
             // FrmSiparis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1374, 644);
+            this.ClientSize = new System.Drawing.Size(1735, 811);
             this.Controls.Add(this.tableLayoutPanelMenuUrun);
             this.Name = "FrmSiparis";
             this.Text = "FrmSiparis";
             this.Load += new System.EventHandler(this.FrmSiparis_Load);
             this.tableLayoutPanelMenuUrun.ResumeLayout(false);
-            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanelMenuUrunler.ResumeLayout(false);
             this.tableLayoutPanelurunler.ResumeLayout(false);
             this.tableLayoutPanelbutonlar.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -203,5 +231,8 @@
         private Button btn_SiparisAl;
         private Button btnAdisyonKapat;
         private Button btnGeri;
+        private TableLayoutPanel tableLayoutPanelMenuUrunler;
+        private TableLayoutPanel tableLayoutPanel1;
+        private ListView lstCart;
     }
 }
